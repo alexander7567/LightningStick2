f43052a6ebc05a35775d7c1ff2cdc012a5f87556
alexander7567 <alex@alexriggs.com>
2013-10-16 11:04:30 -0500

    Initial Commit

---
+++ src/main/java/com/alexriggs/bukkit/lightningstick2/LightningStick2.java
@@ -0,0 +1,84 @@
+package com.alexriggs.bukkit.lightningstick2;
+
+
+import java.util.Random;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.LightningStrike;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LightningStick2 extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		getLogger().info("LightningStick enabled. Visit https://alexriggs.com/viewforum.php?f=150 for support!");
+		getServer().getPluginManager().registerEvents(this, this);
+		this.saveDefaultConfig();
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("lightningstick")){
+			if (args.length == 0) {
+				sender.sendMessage("You must enter a command.");
+			return true;
+			} else {
+				if (args[0].equalsIgnoreCase("reload")) {
+					this.reloadConfig();
+					sender.sendMessage("Configuration file has been reloaded.");
+				} else {
+					sender.sendMessage("Command not found.");
+				}
+			}
+		}
+		return false; 
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onPlayerUse(PlayerInteractEvent event) {
+		Player p = event.getPlayer();
+		if (p.hasPermission("lightningstick.use")) {
+
+			if (event.getAction().equals(Action.LEFT_CLICK_AIR)
+					|| event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+				if (p.getItemInHand().getType() == Material.STICK) {
+					if (this.getConfig().getBoolean("effectOnly")) {
+						LightningStrike lightning = p.getWorld().strikeLightningEffect(p.getTargetBlock(null, 500).getLocation());
+					} else {
+						LightningStrike lightning = p.getWorld().strikeLightning(p.getTargetBlock(null, 500).getLocation());
+					}
+
+				}
+			} else if (event.getAction().equals(Action.RIGHT_CLICK_AIR)
+					|| event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+				Random generator = new Random();
+				if (p.getItemInHand().getType() == Material.STICK) {
+					int i = 0;
+					while (i < this.getConfig().getInt("strikesOnRightClick")) {
+						Location loc = p.getTargetBlock(null, 500).getLocation();
+						loc.setX(loc.getX() + generator.nextInt(10) - 5);
+						loc.setZ(loc.getZ() + generator.nextInt(10) - 5);
+						if (this.getConfig().getBoolean("effectOnly")) {
+							LightningStrike lightning = p.getWorld().strikeLightningEffect(loc);
+						} else {
+							LightningStrike lightning = p.getWorld().strikeLightning(loc);
+						}
+						i++;
+					}
+				}
+			}
+		}
+	}
+}
+
